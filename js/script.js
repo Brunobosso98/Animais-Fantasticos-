ec2dfaa03341da41d015aede6559170a2d51f85f
brunobossomartins1@gmail.com <brunobossomartins1@gmail.com>
2024-11-15 09:56:12 -0300

    adicionado slide de fotos

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ import Funcionamento from "./modules/funcionamento.js";
 import fetchAnimais from "./modules/fetch-animais.js";
 import fetchBtc from "./modules/fetch-btc.js";
 import ScrollAnima from "./modules/scroll-anima.js";
+import SlideNav from "./modules/slide.js";
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
@@ -47,6 +48,10 @@ funcionamento.init();
 fetchAnimais("../../animaisapi.json", ".numeros-grid");
 
 fetchBtc("https://blockchain.info/ticker", ".btc-preco");
+
+const slide = new SlideNav(".slide", ".slide-wrapper");
+slide.init();
+slide.addControl(".custom-controls");
 
 import $ from "jquery";
 import _ from "lodash";
